d3876e8c1d38739f94eaa9ff40e41d0a1ad986b7
susmita000 <susmitapanigrahi6@gmail.com>
2021-03-26 00:03:38 +0530

    first commit to repository

---
+++ src/com/nt/practice/GitDemoTest.java
@@ -0,0 +1,14 @@
+package com.nt.practice;
+
+public class GitDemoTest {
+
+	public static void main(String[] args) {
+		System.out.println("demo test");
+		System.out.println("added for second time");
+	
+		System.out.println("added for third time");
+		System.out.println("added for fourth time");
+
+	}
+
+}
